24a1782d339a2e59408c2d170125ee0fd66e0be8
Volha Soika <volhasoika@gmail.com>
2020-04-06 16:59:42 +0300

    refactor: moving business logic in *.service.js files

--- src/resources/boards/board.service.js
+++ src/resources/boards/board.service.js
@@ -1,5 +1,83 @@
 const usersRepo = require('./board.memory.repository');
+const Board = require('./board.model');
+const All_BOARDS = require('./board.memory.repository');
+const ALL_TASKS = require('../tasks/task.memory.repository');
 
-const getAll = () => usersRepo.getAll();
+const getAll = async (req, res) => {
+  const boards = await usersRepo.getAll();
 
-module.exports = { getAll };
+  if (boards.length) {
+    await res.json(boards.map(Board.toResponse));
+  } else {
+    res.status(404).send({ error: 'The boards not found' });
+  }
+};
+
+const addNewBoard = async (req, res) => {
+  const newBoard = new Board(req.body);
+  All_BOARDS.boards.push(newBoard);
+  await res.json(Board.toResponse(newBoard));
+};
+
+const getBoardById = async (req, res) => {
+  const id = req.params.id;
+  const board = All_BOARDS.boards.find(item => item.id === id);
+
+  if (board) {
+    await res.json(Board.toResponse(board));
+  } else {
+    res.status(404).send({ error: `The board with id ${id} doesn't exist` });
+  }
+};
+
+const updateBoard = async (req, res) => {
+  const id = req.params.id;
+  const board = All_BOARDS.boards.find(item => item.id === id);
+  const updatedBoard = req.body;
+
+  if (board) {
+    for (const key in board) {
+      if (updatedBoard[key]) {
+        board[key] =
+          board[key] !== updatedBoard[key] ? updatedBoard[key] : board[key];
+      }
+    }
+
+    await res.json(`The board with id ${id} have been updated successfully`);
+  } else {
+    res.status(404).send({ error: `The board with id ${id} doesn't exist` });
+  }
+};
+
+const deleteBoard = async (req, res) => {
+  const id = req.params.id;
+  const board = All_BOARDS.boards.find(item => item.id === id);
+
+  if (board) {
+    All_BOARDS.boards.forEach((item, i) => {
+      if (item.id === id) {
+        All_BOARDS.boards.splice(i, 1);
+      }
+    });
+
+    ALL_TASKS.tasks.forEach((item, i) => {
+      if (item.boardId === id) {
+        ALL_TASKS.tasks.splice(i, 1);
+      }
+    });
+
+    await res.json(`The board with id ${id} have been deleted successfully`);
+  } else {
+    await res
+      .status(404)
+      .send({ error: `The board with id ${id} doesn't exist` });
+  }
+};
+
+module.exports = {
+  getAll,
+  addNewBoard,
+  getBoardById,
+  updateBoard,
+  deleteBoard
+};
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,5 +1,73 @@
 const usersRepo = require('./user.memory.repository');
+const User = require('./user.model');
+const All_USERS = require('./user.memory.repository');
+const ALL_TASKS = require('../tasks/task.memory.repository');
 
 const getAll = () => usersRepo.getAll();
 
-module.exports = { getAll };
+const addNewUser = async (req, res) => {
+  const newUser = new User(req.body);
+  All_USERS.users.push(newUser);
+  await res.json(User.toResponse(newUser));
+};
+
+const getUserById = async (req, res) => {
+  const id = req.params.id;
+  const user = All_USERS.users.find(item => item.id === id);
+
+  if (user) {
+    await res.json(User.toResponse(user));
+  } else {
+    res.status(404).send({ error: `The user with id ${id} doesn't exist` });
+  }
+};
+
+const updateUser = async (req, res) => {
+  const id = req.params.id;
+  const user = All_USERS.users.find(item => item.id === id);
+  const updatedUser = req.body;
+
+  if (user) {
+    for (const key in user) {
+      if (updatedUser[key]) {
+        user[key] =
+          user[key] !== updatedUser[key] ? updatedUser[key] : user[key];
+      }
+    }
+
+    await res.json(`The user ${user.name} have been updated successfully`);
+  } else {
+    res.status(404).send({ error: `The user with id ${id} doesn't exist` });
+  }
+};
+
+const deleteUser = async (req, res) => {
+  const id = req.params.id;
+  const user = All_USERS.users.find(item => item.id === id);
+
+  if (user) {
+    All_USERS.users.forEach((item, i) => {
+      if (item.id === id) {
+        All_USERS.users.splice(i, 1);
+
+        ALL_TASKS.tasks.forEach(task => {
+          if (task.userId === id) {
+            task.userId = null;
+          }
+        });
+      }
+    });
+
+    await res.json(`The user ${user.name} have been deleted successfully`);
+  } else {
+    res.status(404).send({ error: `The user with id ${id} doesn't exist` });
+  }
+};
+
+module.exports = {
+  getAll,
+  addNewUser,
+  getUserById,
+  updateUser,
+  deleteUser
+};
